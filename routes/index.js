52c9c991f7fd8e3b40e9eb45058b30395b3e6503
Rincorpes <rincorpes@gmail.com>
2020-01-31 12:18:42 -0500

    unizend-localbtc package.json content loaded in the app HTML meta tags added with the unizend-localbtc package.json content

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+const unizendLocalbtcPJson = require('unizend-localbtc/package.json')
+
+// console.log(unizendLocalbtcPJson)
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: unizendLocalbtcPJson.name,
+    description: unizendLocalbtcPJson.description,
+    keywords: unizendLocalbtcPJson.keywords.toString(),
+    author: unizendLocalbtcPJson.author,
+  });
 });
 
 module.exports = router;
